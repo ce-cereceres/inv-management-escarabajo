9af24852f1e71a0279fba342f560fe1fd72be5f1
Carlos Cereceres <165350098+ce-cereceres@users.noreply.github.com>
2024-06-08 22:26:52 -0600

    Created relationships

--- app/Models/Transfer.php
+++ app/Models/Transfer.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transfer extends Model
 {
     use HasFactory;
+
+    public function sourceWarehouse(): BelongsTo
+    {
+        return $this->belongsTo(Warehouse::class, 'source_warehouse_id');
+    }
+
+    public function destinationWarehouse(): BelongsTo
+    {
+        return $this->belongsTo(Warehouse::class, 'destination_warehouse_id');
+    }
 }
--- app/Models/Warehouse.php
+++ app/Models/Warehouse.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Warehouse extends Model
 {
@@ -29,5 +30,15 @@ class Warehouse extends Model
     public function products(): BelongsToMany
     {
         return $this->belongsToMany(Product::class)->withPivot('quantityAvailable','minimumStockLevel','maximumStockLevel','reoredPoint')->withTimestamps();
+    }
+
+    public function transfersSent(): HasMany
+    {
+        return $this->hasMany(Transfer::class, 'source_warehouse_id');
+    }
+
+    public function transfersReceived(): HasMany
+    {
+        return $this->hasMany(Transfer::class, 'destination_warehouse_id');
     }
 }
