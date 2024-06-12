8a70cedc1ab836606607b435155fc4a62e066cb7
Carlos Cereceres <165350098+ce-cereceres@users.noreply.github.com>
2024-06-11 23:09:30 -0600

    Updated profile information

--- resources/views/shared/transfers-edit-form.blade.php
+++ resources/views/shared/transfers-edit-form.blade.php
@@ -13,23 +13,30 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <form action="{{route('transfers.store')}}" method="POST">
+                    
+                    <form class="max-w-md mx-auto" action="{{route('transfers.store')}}" method="POST">
                         @csrf
-                        <label for="source_warehouse_id">Almacen de origen</label>
-                        <select name="source_warehouse_id" id="source_warehouse_id">
-                            @foreach ($warehouses as $warehouse)
-                                <option value="{{$warehouse->id}}">{{$warehouse->name}}</option>
-                            @endforeach
-                        </select>
-                        <br>
-                        <label for="destination_warehouse_id">Almacen de destino</label>
-                        <select name="destination_warehouse_id" id="destination_warehouse_id">
-                            @foreach ($warehouses as $warehouse)
-                                <option value="{{$warehouse->id}}">{{$warehouse->name}}</option>
-                            @endforeach
-                        </select>
-                        <br>
-                        {{-- LISTA DE PRODUCTOS A ENVIAR --}}
+                        {{-- Almacen de origen --}}
+                        <div class="relative z-0 w-full mb-5 group">
+                            <select name="source_warehouse_id" id="source_warehouse_id" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-600 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" ">
+                                @foreach ($warehouses as $warehouse)
+                                    <option value="{{$warehouse->id}}">{{$warehouse->name}}</option>
+                                @endforeach
+                            </select>
+                            <label for="source_warehouse_id" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Almacén de origen</label>
+                        </div>
+
+                        {{-- Almacen de destino --}}
+                        <div class="relative z-0 w-full mb-5 group">
+                            <select name="destination_warehouse_id" id="destination_warehouse_id" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-600 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" ">
+                                @foreach ($warehouses as $warehouse)
+                                    <option value="{{$warehouse->id}}">{{$warehouse->name}}</option>
+                                @endforeach
+                            </select>
+                            <label for="destination_warehouse_id" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Almacén de destino</label>
+                        </div>
+
+                        {{-- Productos a enviar --}}
                         <div id="select-container">
                             <div id="select">
                                 <select name="product[]" id="product_id">
@@ -43,8 +50,12 @@
                         </div>
                         
                         <br>
-                        <button id="add-select" type="button">Add Select Field</button>
-                        <button type="submit" id="submit_button">Crear</button>
+                        <button id="add-select" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Añadir nuevo producto</button>
+                        <br>
+                        <br>
+                        
+                        
+                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Crear Transferencia</button>
                     </form>
                 </div>
             </div>
