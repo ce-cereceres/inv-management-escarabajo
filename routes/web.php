d93ffb13be0e999e0a7eb509910335dfeeb880ff
Carlos Cereceres <165350098+ce-cereceres@users.noreply.github.com>
2024-06-12 19:49:39 -0600

    Group middlewares

--- routes/web.php
+++ routes/web.php
@@ -18,69 +18,68 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    /* Products */
+    // Index
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+
+    // Create
+    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+
+    // Store
+    Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+
+    // Show
+    Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+
+    // Edit
+    Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+
+    // Update
+    Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+
+    // Destroy
+    Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+
+
+    /* Warehouses */
+    // Index
+    Route::get('/warehouses', [WarehouseController::class, 'index'])->name('warehouses.index');
+
+    // Create
+    Route::get('/warehouses/create', [WarehouseController::class, 'create'])->name('warehouses.create');
+
+    // Store
+    Route::post('/warehouses', [WarehouseController::class, 'store'])->name('warehouses.store');
+
+    // Edit
+    Route::get('/warehouses/{warehouse}/edit', [WarehouseController::class, 'edit'])->name('warehouses.edit');
+
+    // Update
+    Route::put('/warehouses/{warehouse}', [WarehouseController::class, 'update'])->name('warehouses.update');
+
+    // Destroy
+    Route::delete('/warehouses/{warehouse}', [WarehouseController::class, 'destroy'])->name('warehouses.destroy');
+
+    /* Transfers */
+    // Index
+    Route::get('/transfers', [TransferController::class, 'index'])->name('transfers.index');
+
+    // Create
+    Route::get('/transfers/create', [TransferController::class, 'create'])->name('transfers.create');
+
+    // Store
+    Route::post('/transfers', [TransferController::class, 'store'])->name('transfers.store');
+
+    // Show
+    Route::get('/transfers/{transfer}', [TransferController::class, 'show'])->name('transfers.show');
+
+    // Confirm
+    Route::put('/transfers/{transfer}', [TransferController::class, 'confirmTransfer'])->name('transfers.confirm');
+
+    // Destroy
+    Route::delete('/transfers/{transfer}', [TransferController::class, 'destroy'])->name('transfers.destroy');
 });
-
-/* Products */
-// Index
-Route::get('/products', [ProductController::class, 'index'])->middleware(['auth', 'verified'])->name('products.index');
-
-// Create
-Route::get('/products/create', [ProductController::class, 'create'])->middleware(['auth', 'verified'])->name('products.create');
-
-// Store
-Route::post('/products', [ProductController::class, 'store'])->middleware(['auth', 'verified'])->name('products.store');
-
-// Show
-Route::get('/products/{product}', [ProductController::class, 'show'])->middleware(['auth', 'verified'])->name('products.show');
-
-// Edit
-Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->middleware(['auth', 'verified'])->name('products.edit');
-
-// Update
-Route::put('/products/{product}', [ProductController::class, 'update'])->middleware(['auth', 'verified'])->name('products.update');
-
-// Destroy
-Route::delete('/products/{product}', [ProductController::class, 'destroy'])->middleware(['auth', 'verified'])->name('products.destroy');
-
-
-/* Warehouses */
-// Index
-Route::get('/warehouses', [WarehouseController::class, 'index'])->middleware(['auth', 'verified'])->name('warehouses.index');
-
-// Create
-Route::get('/warehouses/create', [WarehouseController::class, 'create'])->middleware(['auth', 'verified'])->name('warehouses.create');
-
-// Store
-Route::post('/warehouses', [WarehouseController::class, 'store'])->middleware(['auth', 'verified'])->name('warehouses.store');
-
-// Edit
-Route::get('/warehouses/{warehouse}/edit', [WarehouseController::class, 'edit'])->middleware(['auth', 'verified'])->name('warehouses.edit');
-
-// Update
-Route::put('/warehouses/{warehouse}', [WarehouseController::class, 'update'])->middleware(['auth', 'verified'])->name('warehouses.update');
-
-// Destroy
-Route::delete('/warehouses/{warehouse}', [WarehouseController::class, 'destroy'])->middleware(['auth', 'verified'])->name('warehouses.destroy');
-
-/* Transfers */
-// Index
-Route::get('/transfers', [TransferController::class, 'index'])->middleware(['auth', 'verified'])->name('transfers.index');
-
-// Create
-Route::get('/transfers/create', [TransferController::class, 'create'])->middleware(['auth', 'verified'])->name('transfers.create');
-
-// Store
-Route::post('/transfers', [TransferController::class, 'store'])->middleware(['auth', 'verified'])->name('transfers.store');
-
-// Show
-Route::get('/transfers/{transfer}', [TransferController::class, 'show'])->middleware(['auth', 'verified'])->name('transfers.show');
-
-// Confirm
-Route::put('/transfers/{transfer}', [TransferController::class, 'confirmTransfer'])->middleware(['auth', 'verified'])->name('transfers.confirm');
-
-// Destroy
-Route::delete('/transfers/{transfer}', [TransferController::class, 'destroy'])->middleware(['auth', 'verified'])->name('transfers.destroy');
-
 
 
 require __DIR__.'/auth.php';
